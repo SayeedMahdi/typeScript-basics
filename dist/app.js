b6bb0c86a6934518a932a4999fb5d14c41a28bfe
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2022-07-25 11:24:26 +0430

    auto-commit

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+var add = function (num1, num2) {
+    return num1;
+};
+var num1 = 12;
+var num2 = "13";
+var result = add({ x: 'asdf' }, 121);
+console.log(result);
+//# sourceMappingURL=app.js.map
