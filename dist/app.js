ad872bf6671664397535e83924efc8d3a5de1781
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2022-07-28 13:22:18 +0430

    all folders stratured

---
+++ dist/app.js
@@ -0,0 +1,12 @@
+class Department {
+    constructor(name) {
+        this.name = name;
+    }
+    add(number1, number2) {
+        return number1 + number2;
+    }
+}
+const science = new Department("Science");
+console.log(science);
+console.log(science.add(12, 23));
+//# sourceMappingURL=app.js.map
---
+++ dist/basics.js
@@ -0,0 +1,8 @@
+var add = (num1, num2) => {
+    return num1;
+};
+var num1 = 12;
+var num2 = 125;
+var result = add({ x: '3' }, 121);
+console.log(result);
+//# sourceMappingURL=basics.js.map
