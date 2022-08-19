81bfc2ffef8fda5510158264e080c019616cbbd7
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2022-08-19 09:44:40 +0430

    new changes commed

---
+++ Department.ts
@@ -0,0 +1,33 @@
+class Department{
+    constructor(private name:String){}
+    }
+    function sum2(add1:number, add2:number): number{
+        return (add1 + add2);
+    }
+    function assignObjects <D extends object,T extends object> (a :D, b:T):Array<D,string> 
+        const obj= Object.assign(a,b);
+    }
+ 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    assignObjects(2,3);
+    sum2(2,3);
+
+
+
+
+
+    
